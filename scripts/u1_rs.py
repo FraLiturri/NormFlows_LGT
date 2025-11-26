5fe36c8646c42b98cfc45be5e97a67ef96d6d727
Linux Secrets <francesco.liturri01@gmail.com>
2025-11-26 18:15:11 +0100

    plot saving added

--- scripts/u1_rs.py
+++ scripts/u1_rs.py
@@ -149,7 +149,7 @@ lqs = np.linspace(lq_2x1.min(), lq_2x1.max(), 100);
 ax.scatter(lq_2x1, lp_2x1, s=5, alpha=0.25);
 ax.plot(lqs, lqs * fit_2x1.slope + fit_2x1.intercept, color='red', zorder=10);
 ax.text(0.15, .85, f"$\\log P = {fit_2x1.slope:.3}\\log q+{fit_2x1.intercept:.3f}$", transform=ax.transAxes);
-ax.savefig(f'u1_rs_logp_logq_beta{beta}_L{L}.png', bbox_inches='tight')
+plt.savefig(f'u1_rs_reg{beta}_L{L}.png', bbox_inches='tight')
 
 lw_2x1 = lp_2x1-lq_2x1
 F_q_2x1, F_q_std_2x1 = torch_bootstrap(-lw_2x1, n_samples=100, binsize=1)
